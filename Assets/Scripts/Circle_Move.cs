a20ebcefb179b9c57a8d73f8cc6ecb25e9cf2b75
Ross1301 <32336848+Ross1301@users.noreply.github.com>
2019-04-30 00:15:25 -0600

    First

---
+++ Assets/Scripts/Circle_Move.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle_Move : MonoBehaviour
+{
+    public float speed = 5;
+    public float direction = 25;
+    public Material mat;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Mathf.Cos(Mathf.Deg2Rad*direction), Mathf.Sin(Mathf.Deg2Rad * direction), 0)*Time.deltaTime*speed;
+       // mat.color = new Color(Random.value, 0, 0);
+        Debug.DrawRay(transform.position - new Vector3(0,0,2), new Vector3(Mathf.Cos(Mathf.Deg2Rad * direction), Mathf.Sin(Mathf.Deg2Rad * direction), 0)- new Vector3(0, 0, 2), Color.red);
+    }
+
+    // called when the cube hits the floor
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (Mathf.Abs(col.contacts[0].normal.x) > 0.5)
+        {
+            direction = 180 - direction + Random.value*50;
+        }
+        else
+            direction = 360 - direction + Random.value * 50;
+        
+    }
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        transform.Rotate(0, 0, 180);
+    }
+}
---
+++ Assets/Scripts/sliders_control.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class sliders_control : MonoBehaviour
+{
+    public Slider speed;
+    public Slider size;
+
+    public Circle_Move Sphere;
+    public Transform Ts_Sphere;
+
+    public GameObject player;
+    public GameObject sliders;
+
+    private bool isShown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Adds a listener to the main slider and invokes a method when the value changes.
+        speed.onValueChanged.AddListener(delegate { speed_Value(); });
+        size.onValueChanged.AddListener(delegate { size_Value(); });
+
+        isShown = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Invoked when the value of the slider changes.
+    public void speed_Value()
+    {
+        //Debug.Log(speed.value);
+        Sphere.speed = 1 + 15 * speed.value;
+        
+    }
+
+    public void size_Value()
+    {
+        float value = 0.1f + 5 * size.value;
+        Ts_Sphere.localScale = new Vector3(value, value, value);
+    }
+
+
+    public void AddPlayer()
+    {
+        GameObject ball = Instantiate(player, new Vector3(0, 0, -10), Quaternion.identity);
+        GameObject slidersp = Instantiate(sliders, new Vector3(100,0,0), Quaternion.identity);
+
+        slidersp.GetComponent<sliders_control>().Sphere = ball.GetComponent<Circle_Move>();
+        slidersp.GetComponent<sliders_control>().Ts_Sphere = ball.transform;
+    }
+
+    public void ShowPlayer()
+    {
+        if (isShown)
+        {
+            isShown = false;
+            player.SetActive(false);
+            sliders.SetActive(false);
+        }
+        else
+        {
+            isShown = true;
+            player.SetActive(true);
+            sliders.SetActive(true);
+        }
+    }
+}
